e06b52a8df88ee8001d511e49a4d710b8fb14b53
Gaurav Virmani <virmanigaurav97@gmail.com>
2024-03-27 20:27:02 +0530

    Adding tests for SkipList

--- mvcc/SkipListNode_test.go
+++ mvcc/SkipListNode_test.go
@@ -19,3 +19,41 @@ func TestPutsAKeyValueAndGetByKeyInNode(t *testing.T) {
 	assert.Equal(t, true, ok)
 	assert.Equal(t, []byte("Hard disk"), value.Slice())
 }
+
+func TestUpdatesTheSameKeyWithADifferentVersion(t *testing.T) {
+	const maxLevel = 8
+	sentinelNode := NewSkipListNode(emptyVersionedKey(), emptyValue(), maxLevel)
+
+	levelGenerator := *utils.NewLevelGenerator(maxLevel)
+	sentinelNode.putOrUpdate(*NewVersionedKey([]byte("HDD"), 1), NewValue([]byte("Hard disk")), levelGenerator)
+	sentinelNode.putOrUpdate(*NewVersionedKey([]byte("HDD"), 2), NewValue([]byte("Hard disk drive")), levelGenerator)
+
+	value, ok := sentinelNode.get(*NewVersionedKey([]byte("HDD"), 3))
+	assert.Equal(t, true, ok)
+	assert.Equal(t, []byte("Hard disk drive"), value.Slice())
+}
+
+func TestGetsTheValueOfAKeyWithTheNearestVersion(t *testing.T) {
+	const maxLevel = 8
+	sentinelNode := NewSkipListNode(emptyVersionedKey(), emptyValue(), maxLevel)
+
+	levelGenerator := *utils.NewLevelGenerator(maxLevel)
+	sentinelNode.putOrUpdate(*NewVersionedKey([]byte("HDD"), 1), NewValue([]byte("Hard disk")), levelGenerator)
+	sentinelNode.putOrUpdate(*NewVersionedKey([]byte("HDD"), 2), NewValue([]byte("Hard disk drive")), levelGenerator)
+
+	value, ok := sentinelNode.get(*NewVersionedKey([]byte("HDD"), 10))
+	assert.Equal(t, true, ok)
+	assert.Equal(t, []byte("Hard disk drive"), value.Slice())
+}
+
+func TestGetsTheValueForNonExistingKey(t *testing.T) {
+	const maxLevel = 8
+	sentinelNode := NewSkipListNode(emptyVersionedKey(), emptyValue(), maxLevel)
+
+	levelGenerator := *utils.NewLevelGenerator(maxLevel)
+	sentinelNode.putOrUpdate(*NewVersionedKey([]byte("HDD"), 1), NewValue([]byte("Hard disk")), levelGenerator)
+	sentinelNode.putOrUpdate(*NewVersionedKey([]byte("HDD"), 2), NewValue([]byte("Hard disk drive")), levelGenerator)
+
+	_, ok := sentinelNode.get(*NewVersionedKey([]byte("Storage"), 1))
+	assert.Equal(t, false, ok)
+}
